b955cb78ec855fc9156635b883b110eb2a8f1e00
hopdev <vanhop.av@gmail.com>
2024-06-10 23:18:27 +0700

    feat: Split book components

---
+++ src/components/book/Bookshelf.js
@@ -0,0 +1,21 @@
+import Book from "./Book";
+
+const Bookshelf = () => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">Currently Reading</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          <li>
+            <Book />
+          </li>
+          <li>
+            <Book />
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default Bookshelf;
